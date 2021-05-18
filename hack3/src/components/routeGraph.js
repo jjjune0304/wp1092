ad98b5370194bead235105caff24169521bc1e32
jjjune0304 <jjsyu0304@gmail.com>
2021-05-18 11:22:47 +0800

    test

---
+++ hack3/src/components/routeGraph.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Station from './station'
+
+function RouteGraph(props) {
+  const data = props.route_data
+  let length = data.length;
+  return (
+    <div className="route-graph-container">
+      {
+        // generate many stations
+        // use <Station /> with your own customized parameters
+        // coding here ...
+        data.map((s, id) => <Station key={s.station_id} s_id={s.station_id} 
+                                s_name={s.station_name} s_type={s.station_type}
+                                end={id === (length-1)} start={id === 0}/>)
+      }
+    </div>
+  )
+}
+
+export default RouteGraph
---
+++ hack3/src/components/station.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Station(props) {
+  const s_id = props.s_id;
+  const s_name = props.s_name;
+  const s_type = props.s_type;
+  const start = props.start;
+  const end = props.end;
+
+  const color = (k) => {
+    if (k === 'R') return 'red';
+    if (k === 'G') return 'green';
+    if (k === 'B') return 'blue';
+    if (k === 'O') return 'orange';
+  }
+  const line = <div id={'l-'+s_id} className={"line"+" "+color(s_type)}></div>;
+  return (
+    <div className="station-line-container">
+      <div id={'s-'+s_id} className={"station-and-name"}> {/* you should add both id and onClick to attributes */
+
+      }
+        <div className={"station-rectangle"+" "+color(s_type)+(end||start?" end":"")}>{s_id}</div>
+        <div className={"station-name"+" "+color(s_type)}>{s_name}</div>
+      </div>
+      {!end?line:""}
+    </div>
+  )
+}
+
+export default Station
